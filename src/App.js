d80bf8d6525e228636f209d67d001a3b4ad32744
pedro <pedrohperoni@gmail.com>
2021-12-03 10:03:39 -0300

    layout components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Footer from "./Components/Footer.js";
 import Header from "./Components/Header.js"
+import Main from "./Components/Main.js";
+import Sidebar from "./Components/Sidebar.js"
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <div className="corpo">
+         <Main />
+         <Sidebar />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import Posts from "./Posts";
+import Stories from "./Stories";
+
+export default function Main(){
+   return(
+      <div class="esquerda">
+          <Stories />
+          <Posts />
+      </div>
+   )
+}
